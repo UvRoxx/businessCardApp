bf44e6bbf3a3a62eff8562de57fa1ba118f7f0ca
Utkarsh Varma <uvroxx2323@gmail.com>
2020-12-18 02:29:46 -0500

    Initial Commit

---
+++ businessCardApp/businessCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessCardAppApp.swift
+//  businessCardApp
+//
+//  Created by UTKARSH VARMA on 2020-12-18.
+//
+
+import SwiftUI
+
+@main
+struct businessCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
